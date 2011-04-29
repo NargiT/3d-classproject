5c17bbf0101d63d773b6f2d928b280bc49fc3db9
martin.matuszek@gmail.com <martin.matuszek@gmail.com@6d9464f5-7e37-6728-6047-94ec794af086>
2011-04-29 22:03:48 +0000

    Actually uploading new files
    git-svn-id: https://mosig1-3d-classproject.googlecode.com/svn/trunk@15 6d9464f5-7e37-6728-6047-94ec794af086

---
+++ fog.h
@@ -0,0 +1,41 @@
+#ifndef FOG_H
+#define FOG_H
+
+#include "object.h"
+
+#include <qdom.h>
+#include <QMap>
+
+using namespace std;
+
+class Fog //: public Object
+{
+public :
+
+  /**
+  * Constructor and destructor
+  */
+  Fog();
+  ~Fog() {}
+
+  /**
+  * Init fog
+  */
+  void init(const string &type, Color fogColor, float val1, float val2=0.0f);
+
+  /**
+  * Enable/Disable fog
+  */
+  void switchFog();
+
+  /**
+  * Parse XML
+  */
+  virtual void initFromDOMElement(const QDomElement& e);
+
+private:
+  Color color;    // color of fog
+};
+
+
+#endif // FOG_H
